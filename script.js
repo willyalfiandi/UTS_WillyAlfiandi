25f61dde4964357fbb3d3f11e89520863ecd5373
willyalfiandi <willyalfiandi.12@gmail.com>
2025-11-15 13:59:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function GantiNama() {
+            document.getElementById("nama").innerText = "Selamat Datang, RUSDINUS ";
+        }
+
+function GantiKelas() {
+            document.getElementById("kelas").innerText = "Kelas JMK_48";
+        }       
+        
+function reset() {
+        document.getElementById("nama").innerText = "Selamat Datang, Nama Mahasiswa";
+        document.getElementById("kelas").innerText = "Kelas 00-00";
+        }
